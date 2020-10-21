febfe3870241cefd30eaa0c693a75454aa492ede
brunhilde <brunhildevink1995@gmail.com>
2020-10-21 17:33:24 +0200

    Updates

---
+++ precache-manifest.b98e48528788e0dedb89719e108d57fa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70fa762b2264c2e6786822ba3d1af195",
+    "url": "/bv-com/index.html"
+  },
+  {
+    "revision": "8e6c8d4f77acda12dac7",
+    "url": "/bv-com/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "57d0a1747646411af5f3",
+    "url": "/bv-com/static/css/main.830cb510.chunk.css"
+  },
+  {
+    "revision": "8e6c8d4f77acda12dac7",
+    "url": "/bv-com/static/js/2.6ae41084.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/bv-com/static/js/2.6ae41084.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57d0a1747646411af5f3",
+    "url": "/bv-com/static/js/main.bc549c47.chunk.js"
+  },
+  {
+    "revision": "800741396d5dba86c9ff",
+    "url": "/bv-com/static/js/runtime-main.d3e23579.js"
+  },
+  {
+    "revision": "6f7b1a09fca7baa10362c913487cb4a0",
+    "url": "/bv-com/static/media/blob.6f7b1a09.svg"
+  }
+]);
